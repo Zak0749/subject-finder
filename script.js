c78f00a98da44af81a6a25e2e0276b66900202a0
Zak <89150707+Zak0749@users.noreply.github.com>
2022-03-31 23:24:30 +0100

    partial matches

--- script.js
+++ script.js
@@ -1,38 +1,90 @@
 let data = [
-  ["maths", "english"],
-  ["english", "maths"],
-  ["maths"],
-  ["maths"],
-  ["maths"],
+  [
+    "Art",
+    "Biology",
+    "English",
+    "Geography",
+    "Human Biology",
+    "Sports & Recreation",
+    "Politics",
+  ],
+  [
+    "Art",
+    "Business Management",
+    "Chemistry",
+    "Engineering Science",
+    "Mathematics",
+    "Photography",
+    "Religious, Moral & Philosophical Studies",
+  ],
+  [
+    "Administration & IT",
+    "English",
+    "French",
+    "History",
+    "Music",
+    "PE",
+    "Physics",
+  ],
+  [
+    "Chemistry",
+    "Geography",
+    "Graphic Communication",
+    "History",
+    "Mathematics",
+    "Travel & Tourism",
+  ],
+  [
+    "Biology",
+    "Business Management",
+    "Computing Science",
+    "Design & Manufacture",
+    "Modern Studies",
+    "Music",
+    "PE",
+  ],
 ];
 
-let subjects = [...new Set(data.flat())];
+let subjects = [...new Set(data.flat())].sort();
 let selects = [];
+
+const closeness = (columns, values) => {
+  const arr = [
+    columns.includes(values[0]),
+    columns.includes(values[1]),
+    columns.includes(values[2]),
+    columns.includes(values[3]),
+    columns.includes(values[4]),
+  ];
+
+  return arr.filter((v) => v).length;
+};
 
 const workOutTable = () => {
   document.getElementById("table").innerHTML = `<tr>
-            <th>Column 1</th>
-            <th>Column 2</th>
-            <th>Column 3</th>
-            <th>Column 4</th>
-            <th>Column 5</th>
+            <th>Column A</th>
+            <th>Column B</th>
+            <th>Column C</th>
+            <th>Column D</th>
+            <th>Column E</th>
         </tr>`;
   let values = selects.map((v) => v.value);
 
   if (values.includes("")) return;
 
   let found = false;
+  let close = [];
 
   data[0].forEach((column_one) => {
     data[1].forEach((column_two) => {
       data[2].forEach((column_three) => {
         data[3].forEach((column_four) => {
           data[4].forEach((column_five) => {
-            if (
-              [column_one, column_two, column_three, column_four, column_five]
-                .sort()
-                .join("") === values.sort().join("")
-            ) {
+            const c = closeness(
+              [column_one, column_two, column_three, column_four, column_five],
+              values
+            );
+            if (c == 5) {
               document.getElementById("table").innerHTML += `<tr>
                 <td>${column_one}</td>
                 <td>${column_two}</td>
@@ -42,6 +94,14 @@ const workOutTable = () => {
               </tr>`;
 
               found = true;
+            } else if (c == 4) {
+              close.push([
+                column_one,
+                column_two,
+                column_three,
+                column_four,
+                column_five,
+              ]);
             }
           });
         });
@@ -50,9 +110,32 @@ const workOutTable = () => {
   });
 
   if (found == false) {
-    document.getElementById(
-      "table"
-    ).innerHTML += `<tr><td>You cant take those together</td></tr>`;
+    document.getElementById("message").innerHTML =
+      "You cant take those together unfortunately here are some partial matches";
+
+    unique = close.reduce((acc, c) => {
+      c.forEach((v, i) => {
+        if (!values.includes(v)) {
+          c[i] = "";
+        }
+      });
+
+      return [...new Set([...acc, c].map((v) => v.join("|")))].map((v) =>
+        v.split("|")
+      );
+    }, []);
+
+    console.log(unique);
+
+    unique.forEach((c) => {
+      document.getElementById("table").innerHTML += `<tr>
+                  <td>${c[0]}</td>
+                  <td>${c[1]}</td>
+                  <td>${c[2]}</td>
+                  <td>${c[3]}</td>
+                  <td>${c[4]}</td>
+                </tr>`;
+    });
   }
 };
 
@@ -72,17 +155,3 @@ window.addEventListener("load", () => {
     selects.push(select);
   }
 });
-
-// console.log(subjects);
-
-// let items = document.getElementsByClassName("subject-input");
-// console.log(items.length);
-// for (let i = 0; i < 5; i++) {
-//   const item = items.item(i);
-//   console.log(item);
-//   for (subject in subjects) {
-//     let option = document.createElement("option");
-//     option.innerText = subject;
-//     item.appendChild(option);
-//   }
-// }
